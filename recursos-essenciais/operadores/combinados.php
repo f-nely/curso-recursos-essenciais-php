7190b40db244bf16c4dcba1d68c036eaa0bc2e4e
nely de faria <nelly.nf54@gmail.com>
2020-12-17 16:40:21 -0300

    Adiciona arquivo combinados.php

---
+++ recursos-essenciais/operadores/combinados.php
@@ -0,0 +1,12 @@
+<?php
+
+$numero = 100;
+$numero += 10;
+
+echo "$numero <br>";
+
+$string = "Treinaweb";
+$string = $string . " Cursos.";
+$string .= " A melhor escola online do Brasil";
+
+echo "$string";
