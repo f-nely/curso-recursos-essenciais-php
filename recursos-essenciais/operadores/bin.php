967a4b3f709edadecf797b8f10450bce8491355d
nely de faria <nelly.nf54@gmail.com>
2020-12-18 17:48:01 -0300

    Adicona arquivo bin.php

---
+++ recursos-essenciais/operadores/bin.php
@@ -0,0 +1,25 @@
+<?php
+
+var_dump(decbin(12)); // converte decimal para binário
+
+var_dump(decbin(9));
+
+var_dump(bindec(1010));
+
+var_dump(12 & 9);
+
+var_dump(12 | 9);
+
+// deslocamento à direita
+
+var_dump(75 >> 1); // >>1
+var_dump(75 >> 2); // >>2
+
+// deslocamento à esquerda
+
+var_dump(75 << 1); // <<1
+
+
+
+
+
