c8c89fb6aafeff2fe87c5eb763822fea3d72c975
nely de faria <nelly.nf54@gmail.com>
2020-12-20 12:25:30 -0300

    Melhorando a leitura

--- recursos-essenciais/tipos/float.php
+++ recursos-essenciais/tipos/float.php
@@ -10,3 +10,7 @@ $n4 = 75e-3; // 75 * 10^-3
 var_dump($n1, $n2, $n3,$n4);
 
 var_dump((.1 + .7) * 10);
+
+var_dump(1_000_000); // para melhorar a leitura. a partir do PHP 7.4.0
+
+var_dump(75_000.98);
