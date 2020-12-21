5bac065ebd5c768fe71c5729fffeb6ab61e284c4
nely de faria <nelly.nf54@gmail.com>
2020-12-21 11:48:50 -0300

    Adiciona arquivo numeros.php

---
+++ recursos-essenciais/funcoes/numeros.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/*
+ * https://www.php.net/manual/pt_BR/about.prototypes.php
+ * https://www.php.net/manual/pt_BR/ref.math.php
+ */
+
+var_dump(abs(-6)); // valor absoluto
+var_dump(abs(6)); // valor absoluto
+var_dump(round(12.4)); // arredonda um número
+var_dump(round(12.6189, 2));
+
+var_dump(ceil(14.1)); // arredonda para cima
+var_dump(floor(14.1)); // arredonda para baixo
+
+var_dump(mt_rand(1, 60)); // gerador melhorado de número aleatórios
+
+var_dump(pow(3,2)); // potência
+
+var_dump(sqrt(9)); // raiz quadrada
+var_dump(sqrt(10));
