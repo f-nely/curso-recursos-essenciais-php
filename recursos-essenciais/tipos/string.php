c387a05c117041123d423c32f60c4b92b5a3495a
nely de faria <nelly.nf54@gmail.com>
2020-12-21 17:35:09 -0300

    Escapando caracteres em strings

--- recursos-essenciais/tipos/string.php
+++ recursos-essenciais/tipos/string.php
@@ -7,3 +7,10 @@ var_dump($nome[10]);
 $nome[10] = "c";
 
 var_dump($nome);
+
+echo "<h1 style='color: darkgrey'>Treinaweb Cursos</h1>";
+echo '<h1 style="color: darkseagreen">Treinaweb Cursos</h1>';
+
+echo "Eu tenho R\$mil reais <br>";
+
+echo "Meu ip é 138.204.212.66//24";
