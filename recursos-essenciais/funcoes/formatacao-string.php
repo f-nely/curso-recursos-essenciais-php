b7b3c70d0929218b2fec40f91abcfc6aeaa727f8
nely de faria <nelly.nf54@gmail.com>
2020-12-22 16:44:25 -0300

    Adiciona arquivo formatacao-string.php

---
+++ recursos-essenciais/funcoes/formatacao-string.php
@@ -0,0 +1,10 @@
+<?php
+
+$curso = "PHP";
+$versao = 7.4;
+
+echo 'Curso de ' , $curso, ' versão ' , $versao;
+echo "<br>";
+echo 'Curso de ' . $curso . ' versão ' . $versao;
+echo "<br>";
+echo "Curso de $curso versão $versao";
