b223f2636e2086fbbfd285b82ba626ac92b19540
nely de faria <nelly.nf54@gmail.com>
2020-12-22 16:27:29 -0300

    Adiciona arquivo string-heredoc.php

---
+++ recursos-essenciais/tipos/string-heredoc.php
@@ -0,0 +1,10 @@
+<?php
+
+// exemplo de delimitação de strings heredoc
+$conteudo = <<<HTML
+    <h1 style="color: lightblue">Olá, webdevelop</h1>
+    <p style="color: burlywood">Bem vindo ao curso de PHP</p>
+HTML;
+
+
+echo $conteudo;
