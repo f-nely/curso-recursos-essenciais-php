a5cb7fd277b9eb35f1dd7893cc234347554fdb81
nely de faria <nelly.nf54@gmail.com>
2020-12-23 20:53:57 -0300

    Adiciona arquivo tratamento-string.php

---
+++ recursos-essenciais/funcoes/tratamento-string.php
@@ -0,0 +1,18 @@
+<?php
+
+var_dump(strtoupper('treinaweb cursos'));
+
+var_dump(strtolower('Treinaweb Cursos'));
+
+var_dump(ucfirst('treinawe cursos'));
+
+var_dump(ucwords('treinaweb cursos online'));
+
+// ltrim - retira espaços em branco (ou outros caracteres) do início da string
+var_dump(ltrim('   treinaweb   '));
+
+// rtrim - retira espaço em branco (ou outros caracteres) do final da string
+var_dump(rtrim('   treinaweb   '));
+
+// trim - retira espaço no ínicio e final de uma string
+var_dump(trim('   treinaweb   '));
