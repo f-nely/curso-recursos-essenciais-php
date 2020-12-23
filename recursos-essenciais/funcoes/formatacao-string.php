d2503d190cc3409602f25a2843129ecf125caf5e
nely de faria <nelly.nf54@gmail.com>
2020-12-23 20:02:17 -0300

    Aprendendo funções para string

--- recursos-essenciais/funcoes/formatacao-string.php
+++ recursos-essenciais/funcoes/formatacao-string.php
@@ -8,3 +8,21 @@ echo "<br>";
 echo 'Curso de ' . $curso . ' versão ' . $versao;
 echo "<br>";
 echo "Curso de $curso versão $versao";
+echo "<br>";
+
+echo sprintf("Curso de %s versão %.1f", $curso, $versao);
+echo "<br>";
+
+printf("Curso de %s versão %.1f", $curso, $versao);
+echo "<br>";
+
+$entrada = ['PHP', 7.4];
+
+$resultado = vsprintf("Curso de %s versão %.1f", $entrada);
+echo $resultado;
+echo "<br>";
+
+vprintf("Curso de %s versão %.1f", $entrada);
+
+
+
