5cd8c3c22a4179998c862ab3750c975959a20d4f
nely de faria <nelly.nf54@gmail.com>
2020-12-24 10:57:46 -0300

    Aprendendo outras funções, str_replace e substr

--- recursos-essenciais/funcoes/manipulacao-string.php
+++ recursos-essenciais/funcoes/manipulacao-string.php
@@ -3,6 +3,27 @@
 $frase = "Treinaweb Cursos";
 
 // strpos - encontra a posição da primeira ocorrência de uma string
-$posicaoR = strpos($frase, "r", 9);
+$posicaoR = strpos($frase, "web");
 
 var_dump($posicaoR);
+
+// str_replace - substitui todas as ocorrências da string de procura com a string de substituição
+$fraseModificada = str_replace('r', '*', $frase);
+
+var_dump($fraseModificada);
+
+$fraseModificada = str_replace(['r', 'w'], '*', $frase);
+var_dump($fraseModificada);
+
+$fraseModificada = str_replace(['r', 'w'], ['*', '#'], $frase);
+var_dump($fraseModificada);
+
+// substr - retorna uma parte de uma string
+$fraseModificada2 = substr($frase, 6, 3);
+var_dump($fraseModificada2);
+
+$fraseModificada2 = substr($frase, -6);
+var_dump($fraseModificada2);
+
+echo (substr('nelynely', 4, 4));
+
