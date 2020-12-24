dcecb54a4664c646063413b11f51ba52387159df
nely de faria <nelly.nf54@gmail.com>
2020-12-24 15:44:24 -0300

    Adiciona arquivo conversao-array.php

---
+++ recursos-essenciais/conversao/conversao-array.php
@@ -0,0 +1,17 @@
+<?php
+
+// convertendo valores para array
+$valor = "TreinaWeb";
+var_dump((array)$valor);
+
+$valor = 1;
+var_dump((array)$valor);
+
+$valor = 1.5;
+var_dump((array)$valor);
+
+$valor = TRUE;
+var_dump((array)$valor);
+
+$valor = NULL;
+var_dump((array)$valor); // Array vazio
