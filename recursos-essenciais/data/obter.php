ed800ccf7df13f44d2cc710151a5ae9c6ad9fd00
nely de faria <nelly.nf54@gmail.com>
2020-12-25 15:54:17 -0300

    Trabalhando com timestamp

--- recursos-essenciais/data/obter.php
+++ recursos-essenciais/data/obter.php
@@ -1,9 +1,27 @@
 <?php
 
-//date_default_timezone_set('America/Sao_Paulo');
+// date_default_timezone_set('America/Sao_Paulo');
 
-$data = date("d/m/Y H:i");
+// $tomorrow = time() + (60 * 60 *24);
+/*
+ * strtotime - interpreta qualquer descrição de
+ * data/hora em texto em inglês em timestamp Unix
+ */
+$tomorrow = strtotime("+1 day");
 
-var_dump($data);
+$data = date("d/m/Y H:i:", $tomorrow);
 
-var_dump(date('e'), date('P'));
+//var_dump($data);
+
+// var_dump(date('e'), date('P'));
+
+//var_dump(time());
+
+function formata_data_para_br($data)
+{
+    $timestamp = strtotime($data);
+
+    return date("d/m/Y", $timestamp);
+}
+
+echo formata_data_para_br('2020-12-31');
