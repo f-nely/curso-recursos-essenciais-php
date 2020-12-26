ff8102ad9e473d5bf6fc47c1e65154458c1af05d
nely de faria <nelly.nf54@gmail.com>
2020-12-26 12:30:37 -0300

    Métodos da classe DateTime

--- recursos-essenciais/data/datetime.php
+++ recursos-essenciais/data/datetime.php
@@ -6,5 +6,24 @@ $othorDate = new DateTime('2020-08-22');
 
 $yesterday = new DateTime('yesterday');
 
-var_dump($now, $othorDate, $yesterday);
+$othorDate2 = DateTime::createFromFormat("d/m/Y H:i:s", "22/08/2020 00:00:00");
 
+//var_dump($othorDate->format("d/m/Y"), $othorDate2);
+
+function iso_para_brasil($date)
+{
+    $dateTime = new DateTime($date);
+
+    return $dateTime->format("d/m/Y");
+}
+
+function brasil_para_iso($date)
+{
+    $dateTime = DateTime::createFromFormat("d/m/Y", $date);
+
+    return $dateTime->format("Y-m-d");
+}
+
+var_dump(iso_para_brasil("2020-08-22"));
+
+var_dump(brasil_para_iso("22/08/2020"));
